c6b7f820a7b7298fb3ccce094431153cc660bdad
2rpadala <rajvja520002@gmail.com>
2023-06-04 14:29:40 +0530

    load balancer

--- main.tf
+++ main.tf
@@ -64,5 +64,15 @@ resource "aws_autoscaling_group" "asg" {
 
 }
 
+// create empty load balancer target group
+resource "aws_lb_target_group" "main" {
+  name     = "${var.name}-${var.env}-tg"
+  port     = 8080
+  protocol = "HTTP"
+  vpc_id   = var.vpc_id
+  tags     = merge(var.tags, { Name = "${var.name}-${var.env}-tg" })
+
+}
+
 
 
